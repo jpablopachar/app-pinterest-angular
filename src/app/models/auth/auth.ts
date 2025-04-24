a73b095a18e900f72acd465da11de026e77c277f
jpablopachar <jpablopachar1993@gmail.com>
2025-04-24 18:22:13 -0500

    feat: agregar interfaces de solicitud y respuesta de autenticación, y mejorar el servicio de autenticación

--- src/app/models/auth/auth.ts
+++ src/app/models/auth/auth.ts
@@ -14,6 +14,17 @@ export interface RegisterRequest {
 }
 
 /**
+ * Representa la solicitud de inicio de sesión de un usuario.
+ *
+ * @property email - Correo electrónico del usuario.
+ * @property password - Contraseña del usuario.
+ */
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+/**
  * Representa la respuesta de autenticación de un usuario.
  *
  * @property {string | Date} createdAt - Fecha de creación del usuario.
@@ -31,5 +42,32 @@ export interface AuthResponse {
   updatedAt: string | Date;
   username: string;
   _id: string;
+  __v: number;
+}
+
+/**
+ * Representa la respuesta del perfil de un usuario.
+ *
+ * @property {_id} string - Identificador único del usuario.
+ * @property {displayName} string - Nombre visible del usuario.
+ * @property {username} string - Nombre de usuario.
+ * @property {email} string - Correo electrónico del usuario.
+ * @property {createdAt} Date | string - Fecha de creación del usuario.
+ * @property {updatedAt} Date | string - Fecha de última actualización del usuario.
+ * @property {followerCount} number - Cantidad de seguidores del usuario.
+ * @property {followingCount} number - Cantidad de usuarios a los que sigue.
+ * @property {isFollowing} boolean - Indica si el usuario actual sigue a este usuario.
+ * @property {__v} number - Versión del documento (usado por MongoDB).
+ */
+export interface ProfileUserResponse {
+  _id: string;
+  displayName: string;
+  username: string;
+  email: string;
+  createdAt: Date | string;
+  updatedAt: Date | string;
+  followerCount: number;
+  followingCount: number;
+  isFollowing: boolean;
   __v: number;
 }
---
+++ src/app/types/actionAuth.ts
@@ -0,0 +1,7 @@
+/**
+ * Representa las acciones posibles de autenticación en la aplicación.
+ * 
+ * - `'login'`: Acción para iniciar sesión.
+ * - `'register'`: Acción para registrar un nuevo usuario.
+ */
+export type ActionAuth = 'login' | 'register';
